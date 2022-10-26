ef20586f7dac9a395e54fdf31b9576e43d1ab2ef
Md Galib Hossain <mdgalib23@gmail.com>
2022-10-26 17:22:32 +0600

    category and courses completed

--- src/Components/Pages/Category/Category.js
+++ src/Components/Pages/Category/Category.js
@@ -1,9 +1,19 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import CourseCard from "../Shared/CourseCard/CourseCard";
 
 const Category = () => {
-  const course = useLoaderData();
-  return <div>course: {course.length}</div>;
+  const Categorycourse = useLoaderData();
+  return (
+    <div>
+      <h4 className="mb-4">
+        Total {Categorycourse.length} course in this Category
+      </h4>
+      {Categorycourse.map((course) => (
+        <CourseCard key={course._id} course={course}></CourseCard>
+      ))}
+    </div>
+  );
 };
 
 export default Category;
--- src/Components/Pages/Courses/Courses.js
+++ src/Components/Pages/Courses/Courses.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Link, useLoaderData } from "react-router-dom";
 
 const Courses = () => {
-  return <div></div>;
+  const course = useLoaderData();
+  console.log(course);
+  const { _id, body, image, idno } = course;
+  return (
+    <div className="card lg:card-side mb-5 bg-base-100 shadow-xl">
+      <figure>
+        <img className="img-fluid" src={image} alt="Album" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{course.name}</h2>
+        <p>{body}</p>
+        <div className="card-actions justify-end">
+          {/* show more on this category */}
+          <Link to={`/category/${idno}`}>
+            <button className="btn btn-primary">
+              Find more on this Category
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Courses;
